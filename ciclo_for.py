00c838377610ef52f0d134a9ee3e04d4a677b265
SALA DE CÓMPUTO <SALA@EAN3373.universidad.ean>
2018-09-18 19:44:12 -0500

    archivo de ciclos}

---
+++ ciclo_for.py
@@ -0,0 +1,53 @@
+# lista = [23,45,15,847]
+
+#Imprime los numeros pares de un rango de 0 a 100
+# for variable in range(0,101):
+#     if variable%2 == 0:
+#         print(variable)
+#
+
+#Imprime los multiplos de 3
+ # for n in range(0,100,3):
+ #     print(n)
+
+#Imprime los numero impares de 0 a 100
+#for k in range(1,94,2):
+#     print(k)
+
+#Crea una lista ramdom e imprime los nimeros de esa lista
+# from random import randint
+# lista = []
+# for p in range(0,randint(0,90)):
+#     lista.append(p)
+# print(lista)
+
+# for a in lista:
+#     print(a)
+
+# def only_int(lista) :
+#     '''
+#
+#     (list) -> list
+#     :param lista: una lista con elementos a azar
+#     :return: list: Una lista con solo numeros enteros
+#
+#
+#     >>> only_int([12,'sdf',212,'gdfg'])
+#     [12, 212]
+#
+#     >>> only_int(['ytyt','gdfg','2','12'])
+#     []
+#     '''
+#
+#     nueva_lista = []
+#     for a in lista:
+#         if type(a) == int:
+#             nueva_lista.append(a)
+#     return nueva_lista
+
+
+
+
+
+
+
