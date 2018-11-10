ced0467b1180e356f2298bffe18ad99e9d175c10
jose <jmcordoba@universidadean.edu.co>
2018-11-10 12:01:41 -0500

    Add Ejercicios matrices con reduccion de matrices y suma de matrices

--- Semana_15/matrices.py
+++ Semana_15/matrices.py
@@ -4,7 +4,7 @@ matriz = [[1,2,3],
           [4,5,6],
           [7,8,9]]
 
-print(matriz)
+print("Imprimiendo una matriz ", matriz)
 
 def matriz_cuadrada(n):
     """
@@ -20,7 +20,7 @@ def matriz_cuadrada(n):
         matriz.append(fila)
     return matriz
 
-print(matriz_cuadrada(4))
+print("Imprimiendo una matriz cuadrada", matriz_cuadrada(4))
 
 def gen_matriz(filas, columnas):
     matriz = []
@@ -31,7 +31,7 @@ def gen_matriz(filas, columnas):
         matriz.append(fila)
     return matriz
 
-print(gen_matriz(3, 5))
+print("Imprimiendo una matriz 3 x 5", gen_matriz(3, 5))
 
 def intercambio_filas(matriz, f1, f2):
     """
@@ -51,7 +51,7 @@ def intercambio_filas(matriz, f1, f2):
     resultante[f2] = fila_temporal
     return resultante
 
-print(intercambio_filas([[1,2],[2,1]], 0, 1))
+print("Imprimiendo ejemplo de intercambio de filas ",intercambio_filas([[1,2],[2,1]], 0, 1))
 
 def producto_escalar(vector, escalar):
     resultante = []
@@ -73,6 +73,37 @@ def suma_vectores(v1, v2):
 def suma_filas(matriz, fila_origen, fila_destino, veces):
     resultante = matriz.copy()
     fila_a_sumar = producto_escalar(resultante[fila_origen], veces)
-    resultante[fila_origen] = suma_vectores(fila_a_sumar, resultante[fila_destino])
+    resultante[fila_destino] = suma_vectores(fila_a_sumar, resultante[fila_destino])
     return resultante
+
+def reducir_matriz(matriz):
+    resultante = matriz.copy()
+    for i in range(0, len(resultante)):
+        resultante[i] = producto_escalar(resultante[i], 1 / resultante[i][i])
+        for j in range(i + 1, len(resultante)):
+            resultante = suma_filas(resultante, i, j, -resultante[j][i])
+    return resultante
+
+print("Imprimiendo Reduccion de una matriz", reducir_matriz([[1, 2],[2, 1]]))
+print("Imprimiendo Reduccion de una matriz", reducir_matriz([[1, 2, 3],[3, 2, 1], [1, 2, 5]]))
+
+def suma_matriz(A, B):
+    resultante = []
+    # Recorremos las filas de la matriz
+    for i in range(0, len(A)):
+        fila = []
+
+        # Recorremos las columnas
+        for j in range (0, len(A[i])):
+
+            # Sumamos los escalares
+            fila.append(A[i][j] + B[i][j])
+        # Insertamos la fila a la matriz
+        resultante.append(fila)
+    return resultante
+
+
+print("Imprimiendo matriz a sumar", matriz)
+print("Imprimiendo Suma por si misma", suma_matriz(matriz, matriz))
+
 
